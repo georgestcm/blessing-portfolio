d93d585e944aabffede94233c6bf77f8a49fd16e
georgestcm <georgestcm@gmail.com>
2024-02-03 13:32:10 +0100

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { ContactComponent } from './component/contact/contact.component';
-import {aboutmeComponent} from './component/aboutme/aboutme.component';
 const routes: Routes = [
     {
         path:'',
@@ -12,10 +11,6 @@ const routes: Routes = [
         path:'contact',
         component: ContactComponent
     },
-    {
-        path:'aboutme',
-        component: aboutmeComponent
-    }
 ];
 
 @NgModule({
