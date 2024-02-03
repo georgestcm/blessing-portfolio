852367510b364009f06485ceb3227f8b4ea32f00
georgestcm <georgestcm@gmail.com>
2024-02-03 16:34:15 +0100

    demo animation

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -8,10 +8,20 @@ import { ContactComponent } from '../contact/contact.component';
 })
 export class HomeComponent implements OnInit {
     showTechListSwitcher: boolean = true;
+    currentProject: string = ''
   constructor(private dialog: MatDialog) { }
   ngOnInit() {
   }
   showTechList(){
   this.showTechListSwitcher = !this.showTechListSwitcher;
   }
+
+
+ hoverOn(value: string){
+    this.currentProject = value;
+ }
+
+ hoverOff(value: string){
+    this.currentProject = value
+ }
 }
