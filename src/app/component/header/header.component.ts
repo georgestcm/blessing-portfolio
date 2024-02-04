e7771fc2a8b02fcecd47db6b1383f81d45f44fc1
georgestcm <georgestcm@gmail.com>
2024-02-04 23:18:42 +0100

    added side bar

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+   showSideBar: boolean = true;
   constructor() { }
 
   ngOnInit() {
   }
 
+  showHideSidebar(){
+    this.showSideBar = !this.showSideBar;
+  }
 }
